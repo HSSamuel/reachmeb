363f1a130295da585acc29075744643f3879daeb
HSSamuel <smkmayomisamuel@gmail.com>
2026-02-23 04:19:52 +0100

    update

--- routes/profiles.js
+++ routes/profiles.js
@@ -44,7 +44,7 @@ router.get("/:username", async (req, res, next) => {
     const profile = await Profile.findOneAndUpdate(
       { username: req.params.username },
       { $inc: { views: 1 } },
-      { new: true },
+      { returnDocument: "after" }
     );
     if (!profile) return res.status(404).json({ error: "Profile not found" });
     res.json(profile);
